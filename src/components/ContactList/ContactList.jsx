6382028479f3ff3faaa9d3b3d1682d92f8b7cf84
AstafievaKateryna <134802379+AstafievaKateryna@users.noreply.github.com>
2024-08-01 19:44:30 +0300

    second

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,5 +1,5 @@
 import Contact from "./Contact/Contact";
-// import s from "./ContactList.module.css";
+import s from "./ContactList.module.css";
 
 const ContactList = ({ contact, handleDelete }) => {
   return (
